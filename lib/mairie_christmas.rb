295b329d9c48f37417b720c1e1e39cde70d40d0c
ao627515 <ao627515@gmail.com>
2024-06-28 07:12:03 +0000

    maj maire_christmas - fonctionne

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -1,74 +1,79 @@
-# require 'nokogiri'
-# require 'open-uri'
+require 'nokogiri'
+require 'open-uri'
 
-# # Cette méthode récupère l'e-mail d'une mairie à partir de son URL
-# def get_townhall_email(townhall_url)
-#   # Ouvre la page web de la mairie
-#   page = Nokogiri::HTML(URI.open(townhall_url))
+# Cette méthode décode une adresse e-mail obfusquée par le mécanisme 'email-protection'
+def decode_email(encoded_string)
+  # La clé de déchiffrement est le premier caractère de la chaîne obfusquée
+  r = encoded_string[0..1].to_i(16)
+  
+  # La chaîne à déchiffrer est tout sauf le premier caractère
+  email = encoded_string[2..-1].scan(/../).map { |e| (e.to_i(16) ^ r).chr }.join
+  
+  email
+end
 
-#   # Utilise XPath pour sélectionner l'e-mail dans le tableau à la 4ème ligne et 2ème colonne
-# #   email = page.xpath('//div[@id="mairie_content"]//div[@class="ville_info"]//p/strong/a')
-#   email = page.xpath('//a')
+# Cette méthode récupère l'e-mail d'une mairie à partir de son URL
+def get_townhall_email(townhall_url)
+  # Ouvre la page web de la mairie
+  page = Nokogiri::HTML(URI.open(townhall_url))
 
-#   puts email.length
-# #   puts email.inspect
-#   puts email['href']
-#   puts
-#   puts
-#   # Retourne l'e-mail
-#   email
-# end
+  # Utilise XPath pour sélectionner l'e-mail dans le tableau à la 4ème ligne et 2ème colonne
+  email_element = page.xpath('//div[@id="mairie_content"]//div[@class="ville_info"]//a')
 
-# # Cette méthode récupère les URLs de toutes les mairies du Val d'Oise
-# def get_townhall_urls
-#   # Ouvre la page web de l'annuaire des mairies du Val d'Oise
-#   page = Nokogiri::HTML(URI.open('https://www.annuaire-mairie.fr/departement-val-d-oise.html'))
+  # Décode l'adresse e-mail obfusquée
+  email = decode_email(email_element[3]['href'].split('#').last)
 
-#   # Initialise un array pour stocker les URLs
-#   urls = []
+  # Retourne l'e-mail
+  email
+end
 
-#   # Sélectionne les liens vers les pages des mairies en utilisant la classe 'lientxt'
-#   page.xpath('//div[@id="intercommunalite_content"]//table[@class="tblmaire"]//tbody//a').each do |link|
-#     # Construit l'URL complète de la mairie
-#     url = "https://www.annuaire-mairie.fr" + link['href']
+# Cette méthode récupère les URLs de toutes les mairies du Val d'Oise
+def get_townhall_urls
+  # Ouvre la page web de l'annuaire des mairies du Val d'Oise
+  page = Nokogiri::HTML(URI.open('https://www.annuaire-mairie.fr/departement-val-d-oise.html'))
 
-#     # Ajoute l'URL à l'array
-#     urls << url
-#   end
+  # Initialise un array pour stocker les URLs
+  urls = []
 
-#   # Retourne l'array des URLs
-#   urls
-# end
+  # Sélectionne les liens vers les pages des mairies en utilisant la classe 'lientxt'
+  page.xpath('//div[@id="intercommunalite_content"]//table[@class="tblmaire"]//tbody//a').each do |link|
+    # Construit l'URL complète de la mairie
+    url = "https://www.annuaire-mairie.fr" + link['href']
 
-# # Cette méthode scrappe les e-mails des mairies du Val d'Oise
-# def fetch_townhall_emails
-#   # Récupère les URLs des mairies
-#   urls = get_townhall_urls
+    # Ajoute l'URL à l'array
+    urls << url
+  end
 
-#   # Initialise un array pour stocker les e-mails et les noms des villes
-#   emails = []
+  # Retourne l'array des URLs
+  urls
+end
 
-#   # Itère sur chaque URL pour récupérer les e-mails
-#   urls.each do |url|
-#     # Récupère l'e-mail de la mairie
-#     # puts url
-#     email = get_townhall_email(url)
-#     break 
+# Cette méthode scrappe les e-mails des mairies du Val d'Oise
+def fetch_townhall_emails
+  # Récupère les URLs des mairies
+  urls = get_townhall_urls
 
-#     # Extrait le nom de la ville de l'URL
-#     town_name = url.split('/').last.split('.').first.capitalize
-#     # town_name = url.text
+  # Initialise un array pour stocker les e-mails et les noms des villes
+  emails = []
 
-#     # Ajoute un hash contenant le nom de la ville et l'e-mail à l'array
-#     emails << { town_name => email }
+  # Itère sur chaque URL pour récupérer les e-mails
+  urls.each do |url|
+    # Récupère l'e-mail de la mairie
+    email = get_townhall_email(url)
 
-#     # Affiche le nom de la ville et l'e-mail pour vérifier que tout fonctionne
-#     # puts "#{town_name}: #{email}"
-#   end
+    # Extrait le nom de la ville de l'URL
+    town_name = url.split('/').last.split('.').first.capitalize
 
-#   # Retourne l'array des e-mails et des noms des villes
-#   emails
-# end
+    # Ajoute un hash contenant le nom de la ville et l'e-mail à l'array
+    emails << { town_name => email }
 
-# # Appelle la méthode pour exécuter le scraping
-# fetch_townhall_emails
+    # Affiche le nom de la ville et l'e-mail pour vérifier que tout fonctionne
+    puts "#{town_name} <=:=> #{email}"
+  end
+
+  # Retourne l'array des e-mails et des noms des villes
+  emails
+end
+
+# Appelle la méthode pour exécuter le scraping
+fetch_townhall_emails
--- spec/mairie_christmas_spec.rb
+++ spec/mairie_christmas_spec.rb
@@ -15,3 +15,28 @@ describe "fetch_townhall_emails" do
     expect(towns).to include('Avernes', 'Ambleville')
   end
 end
+
+describe "decode_email" do
+  it "should decode the obfuscated email correctly" do
+    encoded_email = "/cdn-cgi/l/email-protection#5f3271323e362d363a1f3a253e31293633333a71392d"
+    decoded_email = decode_email(encoded_email.split('#').last)
+    expect(decoded_email).to eq('expected_email@example.com') # Remplacez par l'e-mail décodé attendu
+  end
+end
+
+describe "get_townhall_email" do
+  it "should return a valid email address for a given townhall URL" do
+    townhall_url = 'https://www.annuaire-mairie.fr/ville-ezanville.html'
+    email = get_townhall_email(townhall_url)
+    expect(email).to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i) # Vérifie si l'email est dans un format valide
+  end
+end
+
+describe "get_townhall_urls" do
+  it "should return an array of URLs" do
+    urls = get_townhall_urls
+    expect(urls).not_to be_empty
+    expect(urls).to all(be_a(String))
+    expect(urls.first).to match(/^https:\/\/www\.annuaire-mairie\.fr\//) # Vérifie si l'URL commence par le bon préfixe
+  end
+end
