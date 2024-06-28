a692d79f785d0592880e8b5d50d1ceedfb0bfc24
ao627515 <ao627515@gmail.com>
2024-06-28 01:54:05 +0000

    maj

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -1,43 +1,74 @@
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
 
-# Cette méthode récupère l'e-mail d'une mairie à partir de son URL
-def get_townhall_email(townhall_url)
-  page = Nokogiri::HTML(URI.open(townhall_url))
-  email = page.xpath('//section[2]//tr[4]/td[2]').text
-  email
-end
+# # Cette méthode récupère l'e-mail d'une mairie à partir de son URL
+# def get_townhall_email(townhall_url)
+#   # Ouvre la page web de la mairie
+#   page = Nokogiri::HTML(URI.open(townhall_url))
 
-# Cette méthode récupère les URLs de toutes les mairies du Val d'Oise
-def get_townhall_urls
-  page = Nokogiri::HTML(URI.open('https://www.annuaire-mairie.fr/departement-val-d-oise.html'))
-  urls = []
+#   # Utilise XPath pour sélectionner l'e-mail dans le tableau à la 4ème ligne et 2ème colonne
+# #   email = page.xpath('//div[@id="mairie_content"]//div[@class="ville_info"]//p/strong/a')
+#   email = page.xpath('//a')
 
-  # Sélectionne les liens vers les pages des mairies
-  page.css('a.lientxt').each do |link|
-    url = "http://annuaire-des-mairies.com" + link['href'][1..-1]
-    urls << url
-  end
+#   puts email.length
+# #   puts email.inspect
+#   puts email['href']
+#   puts
+#   puts
+#   # Retourne l'e-mail
+#   email
+# end
 
-  urls
-end
+# # Cette méthode récupère les URLs de toutes les mairies du Val d'Oise
+# def get_townhall_urls
+#   # Ouvre la page web de l'annuaire des mairies du Val d'Oise
+#   page = Nokogiri::HTML(URI.open('https://www.annuaire-mairie.fr/departement-val-d-oise.html'))
 
-# Cette méthode scrappe les e-mails des mairies du Val d'Oise
-def fetch_townhall_emails
-  urls = get_townhall_urls
-  emails = []
+#   # Initialise un array pour stocker les URLs
+#   urls = []
 
-  urls.each do |url|
-    email = get_townhall_email(url)
-    town_name = url.split('/').last.split('.').first.capitalize
-    emails << { town_name => email }
+#   # Sélectionne les liens vers les pages des mairies en utilisant la classe 'lientxt'
+#   page.xpath('//div[@id="intercommunalite_content"]//table[@class="tblmaire"]//tbody//a').each do |link|
+#     # Construit l'URL complète de la mairie
+#     url = "https://www.annuaire-mairie.fr" + link['href']
 
-    # Affiche le nom de la ville et l'e-mail pour vérifier que tout fonctionne
-    puts "#{town_name}: #{email}"
-  end
+#     # Ajoute l'URL à l'array
+#     urls << url
+#   end
 
-  emails
-end
+#   # Retourne l'array des URLs
+#   urls
+# end
 
-# Appelle la méthode pour exécuter le scraping
-fetch_townhall_emails
+# # Cette méthode scrappe les e-mails des mairies du Val d'Oise
+# def fetch_townhall_emails
+#   # Récupère les URLs des mairies
+#   urls = get_townhall_urls
+
+#   # Initialise un array pour stocker les e-mails et les noms des villes
+#   emails = []
+
+#   # Itère sur chaque URL pour récupérer les e-mails
+#   urls.each do |url|
+#     # Récupère l'e-mail de la mairie
+#     # puts url
+#     email = get_townhall_email(url)
+#     break 
+
+#     # Extrait le nom de la ville de l'URL
+#     town_name = url.split('/').last.split('.').first.capitalize
+#     # town_name = url.text
+
+#     # Ajoute un hash contenant le nom de la ville et l'e-mail à l'array
+#     emails << { town_name => email }
+
+#     # Affiche le nom de la ville et l'e-mail pour vérifier que tout fonctionne
+#     # puts "#{town_name}: #{email}"
+#   end
+
+#   # Retourne l'array des e-mails et des noms des villes
+#   emails
+# end
+
+# # Appelle la méthode pour exécuter le scraping
+# fetch_townhall_emails
